ad9270759c7bfdf6137cccf70bd914f2de2fcb2e
DEEPESH BATRA <deepeshbatra589@gmail.com>
2026-07-01 16:53:56 +0530

    Add files via upload

---
+++ variable/basic variable and data types.py
@@ -0,0 +1,8 @@
+name="deepesh"
+age = 21
+height=5.6
+student_status=True
+print("name:",type(name))
+print("age:",type(age))
+print("height:",type(height))
+print("student_status:",type(student_status))
