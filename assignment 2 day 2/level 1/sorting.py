6d234e5c4a3cfc41c2b0959cd29554c39e9398c0
Deepesh2575 <deepeshbatra589@gmail.com>
2026-07-02 16:03:22 +0530

    adding new file

--- assignment 2 day 2/level 1/sorting.py
+++ assignment 2 day 2/level 1/sorting.py
@@ -6,3 +6,7 @@ a.sort()
 print("the ascending order:",a)
 a.sort(reverse=True)
 print("the desending order:",a)
+
+print("the maximam number is :",max(a))
+print("the minimum number is :",min(a))
+print("the sum of list is:",sum(a))
